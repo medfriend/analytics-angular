ae8365dd484d593ae4201a5ac952a5b81b598a46
axlMax <axlyeyo2000@gmail.com>
2024-08-02 23:33:56 -0500

    feature/create control panel

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { ControlPanelComponent } from './components/control-panel/control-panel.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [ 
-    HeaderComponent
+    HeaderComponent,
+    ControlPanelComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
