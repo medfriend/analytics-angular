cc86aa93501438471da71aee50b4605a031a6f58
axlMax <axlyeyo2000@gmail.com>
2024-08-03 00:25:04 -0500

    feature/advance in the control panel part

--- src/app/views/home/components/control-panel/control-panel.component.ts
+++ src/app/views/home/components/control-panel/control-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-control-panel',
   standalone: true,
-  imports: [],
+  imports: [ MatIconModule ],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.scss'
 })
