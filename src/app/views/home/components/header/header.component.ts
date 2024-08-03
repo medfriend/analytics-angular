5d1c2ba6217a1ba3ab16f6e3930b4e8b97f6f816
axlMax <axlyeyo2000@gmail.com>
2024-08-02 23:09:49 -0500

    feature/complete the down arrow user animation

--- src/app/views/home/components/header/header.component.ts
+++ src/app/views/home/components/header/header.component.ts
@@ -1,13 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ MatIconModule ],
+  imports: [ MatIconModule, CommonModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isUserArrowActive: boolean = false;
 
+  userArrowActive(){
+    this.isUserArrowActive = true
+  }
+
+  userArrowDeactive(){
+    this.isUserArrowActive = false
+  }
 }
