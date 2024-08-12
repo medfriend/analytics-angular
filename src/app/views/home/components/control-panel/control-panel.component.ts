28cbbe45decd0115824f7babc0d268b82e221066
axlMax <axlyeyo2000@gmail.com>
2024-08-11 22:10:33 -0500

    feature/creater the animatio for the icon

--- src/app/views/home/components/control-panel/control-panel.component.ts
+++ src/app/views/home/components/control-panel/control-panel.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-control-panel',
   standalone: true,
-  imports: [ MatIconModule ],
+  imports: [ 
+    MatIconModule,
+    CommonModule
+  ],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.scss'
 })
 export class ControlPanelComponent {
+  showActions = false;
 
+  ActiveShowAction(){
+    this.showActions = true
+    console.log("entro")
+  }
+
+  DeactiveShowAction(){
+    this.showActions = false
+    console.log("salio")
+  }
 }
