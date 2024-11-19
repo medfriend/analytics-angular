a83576315a71e20fe357efa120510951334440b3
axlMax <axlyeyo2000@gmail.com>
2024-11-18 23:48:34 -0500

    agregar estilos al autocomplete componente

--- src/app/components/autocompletes/basic-autocomplete.component.ts
+++ src/app/components/autocompletes/basic-autocomplete.component.ts
@@ -1,9 +1,11 @@
 import {Component} from "@angular/core";
+import {sharedModules} from "../../shared/shared.module";
 
 @Component({
   selector: 'app-basic-autocomplete',
   styleUrls: ['./basic-autocomplete.component.scss'],
   templateUrl: './basic-autocomplete.component.html',
   standalone: true,
+  imports: [ [...sharedModules] ]
 })
 export class BasicAutocompleteComponent {}
