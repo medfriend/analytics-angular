31266c8bf7957eb00364e19348ec9c78b2a6bfcc
axlMax <axlyeyo2000@gmail.com>
2024-12-01 00:41:27 -0500

    refactorizacion de la funcionalidad de tabla

--- src/app/components/table/basic-table/table.component.ts
+++ src/app/components/table/basic-table/table.component.ts
@@ -6,6 +6,8 @@ import {UserListComponent} from "../../list/user-list/user-list.component";
 import {TableListComponent} from "../../list/table-list/table-list.component";
 import {TableColumn} from "../../../core/interfaces/components/table/basic-table/basic-table.interface";
 import {enviroment} from "../../../enviroment/components.enviroment";
+import {BasicPaginationComponent} from "../../pagination/basic-pagination/basic-pagination.component";
+import {TableService} from "../../../util/table/table.service";
 
 
 @Component({
@@ -13,9 +15,9 @@ import {enviroment} from "../../../enviroment/components.enviroment";
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
   standalone: true,
-  imports: [[...sharedModules], ActionPanelComponent, UserListComponent, TableListComponent]
+  imports: [[...sharedModules], ActionPanelComponent, UserListComponent, TableListComponent, BasicPaginationComponent]
 })
-export class TableComponent implements OnChanges, AfterViewInit {
+export class TableComponent implements AfterViewInit {
   @ViewChild('tableContainer', { static: true }) tableContainer!: ElementRef;
   @Input() columns: TableColumn[] = [];
   @Input() data: any[] = [];
@@ -24,17 +26,11 @@ export class TableComponent implements OnChanges, AfterViewInit {
 
   @Output() overflowDetected: EventEmitter<boolean>  = new EventEmitter<boolean>();
 
-  //TODO guardar esta variable dentro de un redux y su copia en el localstorage
-  currentPage: number = 1;
-  itemsPerPage: number = enviroment.numberRowsTable;
-  paginatedData: any[] = [];
-  totalPages: number = 0;
+  constructor(
+    protected tableService: TableService,
+  ) {}
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['data'] || changes['columns']) {
-      this.calculatePagination();
-    }
-  }
+  paginatedData: any[] = [];
 
   ngAfterViewInit(): void {
     this.checkOverflow();
@@ -53,44 +49,8 @@ export class TableComponent implements OnChanges, AfterViewInit {
     }
   }
 
-  isBoolean(value: any): boolean {
-    return typeof value === 'boolean';
+  handlerPagination(data: any[]){
+    this.paginatedData = data
   }
 
-  calculatePagination() {
-    if (!this.paginated) {
-      this.paginatedData = this.data;
-      return;
-    }
-
-    this.totalPages = Math.ceil(this.data.length / this.itemsPerPage);
-    this.currentPage = Math.min(this.currentPage, this.totalPages);
-
-    const start = (this.currentPage - 1) * this.itemsPerPage;
-    const end = this.currentPage * this.itemsPerPage;
-    this.paginatedData = this.data.slice(start, end);
-  }
-
-  convertDate(dateString : string): string{
-    const date = new Date(dateString);
-
-    const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    };
-
-    return date.toLocaleString('es-ES', options);
-  }
-
-  changePage(direction: number) {
-    const newPage = this.currentPage + direction;
-    if (newPage >= 1 && newPage <= this.totalPages) {
-      this.currentPage = newPage;
-      this.calculatePagination();
-    }
-  }
 }
--- src/app/components/table/mini-table/mini-table.component.ts
+++ src/app/components/table/mini-table/mini-table.component.ts
@@ -1,86 +1,40 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from "@angular/core";
+import {Component, Input} from "@angular/core";
 import {BasicBorderComponent} from "../../border/basic-border.component";
 import {TableColumn} from "../../../core/interfaces/components/table/basic-table/basic-table.interface";
-import {enviroment} from "../../../enviroment/components.enviroment";
-import {NgForOf, NgIf} from "@angular/common";
 import {BasicCollapsedComponent} from "../../collapsed/basic-collapsed/basic-collapsed.component";
 import {sharedModules} from "../../../shared/shared.module";
 import {ActionPanelComponent} from "../../actionPanel/actionPanel.component";
 import {TableListComponent} from "../../list/table-list/table-list.component";
+import {BasicPaginationComponent} from "../../pagination/basic-pagination/basic-pagination.component";
+import {TableService} from "../../../util/table/table.service";
 
 @Component({
   selector: "app-mini-table",
   templateUrl: "./mini-table.component.html",
   styleUrls: ["./mini-table.component.scss"],
   standalone: true,
-  imports: [
-    BasicBorderComponent,
-    BasicCollapsedComponent,
-    [...sharedModules],
-    ActionPanelComponent,
-    TableListComponent
-  ]
+    imports: [
+        BasicBorderComponent,
+        BasicCollapsedComponent,
+        [...sharedModules],
+        ActionPanelComponent,
+        TableListComponent,
+        BasicPaginationComponent
+    ]
 })
-export class miniTableComponent implements OnInit, OnChanges {
+export class miniTableComponent {
   @Input() columns: TableColumn[] = [];
   @Input() data: any[] = [];
   @Input() paginated: boolean = true;
   @Input() idKey: string = '';
 
-  currentPage: number = 1;
-  itemsPerPage: number = enviroment.numberRowsTable;
+  constructor(
+    protected tableService: TableService,
+  ) {}
+
   paginatedData: any[] = [];
-  totalPages: number = 0;
 
-  ngOnInit() {
-    console.log(this.data)
+  handlerPagination(data: any[]){
+    this.paginatedData = data
   }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['data'] || changes['columns']) {
-      this.calculatePagination();
-    }
-  }
-
-  isBoolean(value: any): boolean {
-    return typeof value === 'boolean';
-  }
-
-  convertDate(dateString : string): string{
-    const date = new Date(dateString);
-
-    const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-    };
-
-    return date.toLocaleString('es-ES', options);
-  }
-
-  changePage(direction: number) {
-    const newPage = this.currentPage + direction;
-    if (newPage >= 1 && newPage <= this.totalPages) {
-      this.currentPage = newPage;
-      this.calculatePagination();
-    }
-  }
-
-  calculatePagination() {
-    if (!this.paginated) {
-      this.paginatedData = this.data;
-      return;
-    }
-
-    this.totalPages = Math.ceil(this.data.length / this.itemsPerPage);
-    this.currentPage = Math.min(this.currentPage, this.totalPages);
-
-    const start = (this.currentPage - 1) * this.itemsPerPage;
-    const end = this.currentPage * this.itemsPerPage;
-    this.paginatedData = this.data.slice(start, end);
-  }
-
 }
