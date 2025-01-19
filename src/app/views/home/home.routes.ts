bb51ed6d5ae1e8b12e454414ca23a1a51f1f218c
axlMax <axlyeyo2000@gmail.com>
2025-01-19 12:04:49 -0500

    crear componente de administracion de recurso

--- src/app/views/home/home.routes.ts
+++ src/app/views/home/home.routes.ts
@@ -3,18 +3,8 @@ import {HomeComponent} from "./views/main/home.component";
 import {AccountComponent} from "./views/account/account.component";
 import {AppearanceComponent} from "./views/appearance/appearance.component";
 import {PreferenceComponent} from "./views/preference/preference.component";
-import {MenuAdminComponent} from "../../feature/admin/menu/menu.component";
-import {RecursoAdminComponent} from "../../feature/admin/recurso/recurso.component";
-import {UsuarioAdminComponent} from "../../feature/admin/usuario/usuario.component";
 import {RolAdminComponent} from "../../feature/admin/rol/rol.component";
 import {ParentMenuComponent} from "./views/main/components/parent-menu/parent-menu.component";
-import {CrearUsuarioComponent} from "../../feature/admin/usuario/pages/crear-usuario/crear-usuario.component";
-import {
-  ActualizarUsuarioComponent
-} from "../../feature/admin/usuario/pages/actualizar-usuario/actualizar-usuario.component";
-import {
-  EliminarUsuarioComponent
-} from "../../feature/admin/usuario/pages/eliminar-usuario/eliminar-usuario.component";
 import {EntidadComponent} from "../../feature/admin/entidad/entidad.component";
 import {RecursoMenuComponent} from "../../feature/asignacion/recurso-menu/recurso-menu.component";
 import {SubmenusMenuComponent} from "../../feature/asignacion/submenus-menu/submenus-menu.component";
@@ -23,8 +13,10 @@ import {PermissionsGuard} from "../../core/guard/permission.guard";
 import {
   GestionMantenimientoComponent
 } from "../../feature/mantenimiento/gestion-mantenimiento/gestion-mantenimiento.component";
-import {CrearRecursoComponent} from "../../feature/admin/recurso/pages/crear-recurso/crear-recurso.component";
 import {CrearEntidadComponent} from "../../feature/admin/entidad/pages/crear-entidad/crear-entidad.component";
+import {ADMIN_USUARIO_ROUTE} from "../../feature/admin/usuario/usuario.route";
+import {ADMIN_RECURSO_ROUTE} from "../../feature/admin/recurso/recurso.route";
+import {ADMIN_MENU_ROUTE} from "../../feature/admin/menu/menu.route";
 
 export const HOME_ROUTES: Routes = [
   {
@@ -33,83 +25,24 @@ export const HOME_ROUTES: Routes = [
     children: [
       { path: 'user/account', component: AccountComponent },
       { path: 'user/appearance', component: AppearanceComponent },
-      { path: 'user/preference', component: PreferenceComponent},
-      { path: 'parent-menu', component: ParentMenuComponent},
-      {
-        path: 'administracion-usuarios',
-        component: UsuarioAdminComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'administracion-usuarios/crear',
-        component: CrearUsuarioComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'administracion-usuarios/actualizar',
-        component: ActualizarUsuarioComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'administracion-usuarios/eliminar',
-        component: EliminarUsuarioComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'administracion-recursos',
-        component: RecursoAdminComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'administracion-recursos/crear',
-        component: CrearRecursoComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'administracion-menus',
-        component: MenuAdminComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'administracion-roles',
-        component: RolAdminComponent,
-        canActivate: [PermissionsGuard]
-      },
+      { path: 'user/preference', component: PreferenceComponent },
+      { path: 'parent-menu', component: ParentMenuComponent },
+      ...ADMIN_USUARIO_ROUTE,
+      ...ADMIN_RECURSO_ROUTE,
+      ...ADMIN_MENU_ROUTE,
+      { path: 'administracion-roles', component: RolAdminComponent, canActivate: [PermissionsGuard] },
       {
         path: 'administracion-entidades',
-        component: EntidadComponent,
-        canActivate: [PermissionsGuard]
+        children: [
+          { path: '', component: EntidadComponent, canActivate: [PermissionsGuard] },
+          { path: 'crear', component: CrearEntidadComponent, canActivate: [PermissionsGuard] },
+        ]
       },
-      {
-        path: 'administracion-entidades/crear',
-        component: CrearEntidadComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'asignacion-menu-recurso',
-        component: RecursoMenuComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'asignacion-menu-submenu',
-        component: SubmenusMenuComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'asignacion-user-rol',
-        component: RolUsuarioComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'gestion-mantenimiento',
-        component: GestionMantenimientoComponent,
-        canActivate: [PermissionsGuard]
-      },
-      {
-        path: 'solicitud-mantenimiento',
-        component: RolUsuarioComponent,
-        canActivate: [PermissionsGuard]
-      },
+      { path: 'asignacion-menu-recurso', component: RecursoMenuComponent, canActivate: [PermissionsGuard] },
+      { path: 'asignacion-menu-submenu', component: SubmenusMenuComponent, canActivate: [PermissionsGuard] },
+      { path: 'asignacion-user-rol', component: RolUsuarioComponent, canActivate: [PermissionsGuard] },
+      { path: 'gestion-mantenimiento', component: GestionMantenimientoComponent, canActivate: [PermissionsGuard] },
+      { path: 'solicitud-mantenimiento', component: RolUsuarioComponent, canActivate: [PermissionsGuard] },
     ]
   }
-]
+];
